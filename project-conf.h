cc6053c877d0bca67b7ce18e0fd25d571698d4d7
Frycus <mateusz.fryze@student.put.poznan.pl>
2017-05-22 00:50:22 +0200

    Add files via upload

---
+++ project-conf.h
@@ -0,0 +1,14 @@
+//RDC channel check rate
+#define NETSTACK_CONF_RDC_CHANNEL_CHECK_RATE 16
+//RDC driver (contikimac_driver, nullrdc_driver, lpp_driver, xmac_driver, cxmac_driver)
+#define NETSTACK_CONF_RDC nullrdc_driver
+//MAC driver 
+#define NETSTACK_CONF_MAC nullmac_driver
+//Input voltage for Tmote Sky sensor connected to the computer USB port
+#define VIN 3
+//Current consuptions for Tmote Sky sensor transmitting, receiving, CPU and LPM
+#define I_TX 0.0195
+#define I_RX 0.0218
+#define I_CPU 0.0018
+#define I_LPM 0.000054
+
